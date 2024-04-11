710b70b1717eae345d6beabb5c2aefd033bda389
Deepali Kerai <deepali.kerai@aiscout.io>
2024-04-11 10:27:08 +0100

    AT-357 Utilise the drill automation utils

--- setup_environment.bat
+++ setup_environment.bat
@@ -1,19 +1,26 @@
 @echo off
 
-:: Step 1: Download and Install Python
+:: Step 1: Download and Install Chocolatey (if not already installed)
+echo Installing Chocolatey...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Step 2: Download and Install Python
 echo Installing Python...
 choco install python --yes
 
-:: Step 2: Create Virtual Environment
+:: Step 3: Create Virtual Environment
 echo Creating Virtual Environment...
 python -m venv snoc_env
 
-:: Step 3: Activate Virtual Environment
+:: Step 4: Activate Virtual Environment
 echo Activating Virtual Environment...
 call snoc_env\Scripts\activate
 
-:: Step 4: Install Required Libraries
+:: Step 5: Install Required Libraries
 echo Installing Required Libraries...
 pip install -r requirements.txt
+
+echo Install Drill Automation Utilties
+pip install git+https://github.com/AiSCOUT/Automation-DrillUtils.git@AT-357
 
 echo Setup complete.
